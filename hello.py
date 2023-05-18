43dcc226eb0ab425dec1b3347cd9805cf5dd8d93
swathi <SwathiKarri21@example.com>
2023-05-18 05:00:04 +0000

    added hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
+def (name):
+    return f"Hello, {name}!"
 
-(name):
-        return f"Hello, {name}!"
-
-    if __name__ == "__main__":
-            name = input("Enter your name: ")
-                print(hello(name))
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(hello(name))
